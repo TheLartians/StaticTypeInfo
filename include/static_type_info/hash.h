070c28f1c480c3e48b04a93d069f75a9958aeb78
Lars Melchior <TheLartians@users.noreply.github.com>
2020-04-26 16:55:07 +0200

    Update comment

--- include/static_type_info/hash.h
+++ include/static_type_info/hash.h
@@ -7,7 +7,7 @@
 namespace static_type_info {
 
   // FNV1a c++11 constexpr compile time hash functions, 32 and 64 bit
-  // str should be a null terminated string literal, value should be left out
+  // str should be implicitly convertible std::string_view, value should be left out
   // e.g hash_32_fnv1a_const("example")
   // code license: public domain or equivalent
   // post: https://notes.underscorediscovery.com/constexpr-fnv1a/
